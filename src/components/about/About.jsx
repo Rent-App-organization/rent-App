9724c30fea567cfde8043e9fe51e2fdf5134bc9c
l2vrns <lorans0alkhalailah@gmail.com>
2025-02-14 15:54:00 +0300

    done

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,3 +1,19 @@
-export default function About() {
-  return <div>About</div>;
+import { Navbar } from '../index.js';
+import OurTeam from './OurTeam';
+import Process from './Process';
+import Hero from './Hero';
+import Footer from "../footer/Footer";
+
+function About() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Process />
+      <OurTeam />
+      <Footer />
+    </>
+  );
 }
+
+export default About;
